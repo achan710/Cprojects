fd14debd68362171066324cdd5fdd23595c60946
achan710 <allanchansf@gmail.com>
2016-02-16 23:12:03 -0800

    Started on Exercise 4-3. Problem set up done.

--- main.c
+++ main.c
@@ -19,29 +19,56 @@ note: to set tabs in vim
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> /*for atof() */
 #include "assert.h"
+#include <ctype.h>
 
+// Preprocessor Constants
 #define TRUE 1
 #define FALSE 0
 
-#define EX4_1 TRUE
-#define EX4_3 FALSE
+#define EX4_1 FALSE
+#define EX4_3 TRUE
 
-int strindex(char s[], char t[]);
+
+// Problem Constants
+#if (EX4_3 == TRUE)
+#define MAXOP 100 /* max size of operand or operator */
+#define NUMBER '0' /* signal that a number was found */
+#define MAXVAL 100  /* maximum depth of val stack */
+#define BUFSIZE 100
+#endif
+
+// Function Declarations
+int strindex(char s[], char t[]); /* Ex4.1 */
 int mystrindex(char s[], char t[]);
+
+void push (double f); /* Ex 4.3 */
+double pop(void);
+int getop(char []);
+int getch(void);
+void ungetch(int);
+int polish_calc();
+
+// Global Variables
+#if (EX4_3 == TRUE)
+int sp = 0; /* next free stack position */
+double val[MAXVAL]; /* value stack */
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buf */
+#endif
+
 
 int main(void)
 {
-#if (FALSE)
+#if (EX4_1 == TRUE)
     // code to test my strindex
     int result;
     result = mystrindex("abceeeeabc", "eab");
     printf("result = %d\n", result);
     assert ( 6 == result);
-#endif
 
-#if (EX4_1 == TRUE)
+    // Ex. 4.1 start here
     assert( 2 == strindex("eex", "x"));     // 1 letter in s
     assert( 5 == strindex("eexeex", "x"));     // 2 letters in s
     assert( 4 == strindex("eexeex", "e"));     // last e
@@ -51,7 +78,7 @@ int main(void)
 #endif
 
 #if (EX4_3 == TRUE)
-	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+polish_calc();
 #endif
 
 	// No errors encountered
@@ -87,6 +114,7 @@ int strindex(char s[], char t[])
 }
 
 /*
+ * EX4_1
  * Implemented my own strindex function.
  *
  * Algorithm:
@@ -144,8 +172,100 @@ int mystrindex(char s[], char t[])
 }
 
 
+/*
+ * Exercise 4-3. Given the basic framework, it's straightforward 
+ * to extend the calculator. Add the modulus (%) operator and 
+ * provisions for negative numbers. c Polish calculator.
+ */
+int polish_calc()
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+    return 0;
+}
 
+/* push: push f onto value stack */
+void push (double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
 
+/* pop: pop and return top value from stack */
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
 
+/* getop: get next character or numeric operand */
+int getop(char s[])
+{
+    int i, c;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c; /* not a number */
+    i = 0;
+    if (isdigit(c)) /* collect integer part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.') /* collect fraction part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
 
+int getch(void) /* get a (possibly pushed-back) character */
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
 
+void ungetch(int c) /* push character back on input */
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
